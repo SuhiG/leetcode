cbc362fd84ffe93004ce4b421ee994c08f384617
Sudeera Gunathilaka <32849685+SuhiG@users.noreply.github.com>
2021-06-06 09:47:37 +0900

    Create Two sum.py

---
+++ Two sum.py
@@ -0,0 +1,11 @@
+#difficulty = easy
+
+class Solution:
+    def twoSum(self, nums, target): 
+        h = {}
+        for i, num in enumerate(nums): # enumerate through the array
+            n = target - num 
+            if n not in h: 
+                h[num] = i # append num to h ex: {0: 2, 1: 7}
+            else:
+                return [h[n], i] # return 
